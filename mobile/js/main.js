7f22805d7d456ff63d9309d2dfb11c93796d5b7c
kssoo27 <coolcool_22@naver.com>
2019-06-09 23:01:53 +0900

    mobile end

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -1,34 +1,29 @@
 window.addEventListener("load", function(){
 /* 화면 비율 조정 */
-	var windowHeight;
-	var windowWidth;
 	$(window).resize(function(){
-		windowHeight=$(window).height();
-		windowWidth=$(window).width();
-	}).trigger("resize");
-
-/*스크롤 이벤트 */
-/*	$(window).scroll(function(){
-		scT=$(window).scrollTop();
-		
-		// Brand Story 스크롤 이벤트
-		$(".conts > li").each(function(){
-			// 각각의 컨텐츠 상단좌표
-			contT=$(this).offset().top;
-		//	console.log(contT-windowHeight);
-		//	console.log(scT);
-			if(scT>contT-windowHeight+100){
-				$(this).find("img").addClass("on");
-			}
-		});
-	}).trigger("scroll");
-*/
+		bannerWidth=mainBannerList[0].clientWidth;
+		mainBanner.classList.add("transNone");
+		mainBanner.style.transform="translatex("+keyIndex*-bannerWidth+"px)";
+	})
+/* header */
+// search 검색기능
+	$(".search > a").click(function(e){
+		e.preventDefault();
+		$(".search_box").slideDown(300);
+		$(".header_op").fadeIn(300);
+	});
+	$(".search_close").click(function(e){
+		e.preventDefault();
+		$(".search_box").slideUp(300);
+		$(".header_op").fadeOut(300);
+	});
 /* Mobile */
 // 메뉴 텝 클릭시 ----------------------------------
 	$(".tab").click(function(e){
 		e.preventDefault();
 		$(".mobile").addClass("active");
-		$(".dim").addClass("active");
+		$(".search_box").slideUp(300);
+		$(".dim").fadeIn(300);
 		$("body").addClass("fixed");
 	});
 	
@@ -47,11 +42,11 @@ window.addEventListener("load", function(){
 	});
 	
 // 닫기버튼
-	$(".close").click(function(e){
+	$(".menu_close").click(function(e){
 		e.preventDefault();
 		$(".mobile").removeClass("active");
 		$(".gnb > li > a").removeClass("active");
-		$(".dim").removeClass("active");
+		$(".dim").fadeOut(300);
 		$(".depth").slideUp();
 		$("body").removeClass("fixed");
 	});
@@ -59,178 +54,103 @@ window.addEventListener("load", function(){
 	$(".dim").click(function(){
 		$(".mobile").removeClass("active");
 		$(".gnb > li > a").removeClass("active");
-		$(this).removeClass("active");
+		$(".dim").fadeOut(300);
 		$(".depth").slideUp();
+		$(".search_box").slideUp(300);
 		$("body").removeClass("fixed");
 	});
 	
-// 검색기능
-	$(".search > a").click(function(e){
-		e.preventDefault();
-		if($(".search_box").is(":visible")){
-			$(".search_box").hide();
-			$(this).children().attr({src:"images/search_icon.png"});
-		}
-		else{
-			$(".search_box").slideDown();
-			$(this).children().attr({src:"images/close_icon.png"});
-			$("#header").addClass("active");
-		}
-	});
-
 /* keyvisual 메인 배너 */
+	var body=document.querySelector("body");
 // 초기 설정
-	var keyIndex=0;
 	var mainBanner=document.querySelector(".mainBanner ul");
 	var mainBannerList=document.querySelectorAll(".mainBanner ul li");
 // 슬라이드 개수
 	var keyTotal=mainBannerList.length;
 	for(i=0; i<keyTotal; i++){
-		mainBannerList[i].style.width=100/(keyTotal+1)+"%";
+		mainBannerList[i].style.width=100/(keyTotal+2)+"%";
 	}
-	mainBanner.style.width=(keyTotal+1)*100+"%";
+	mainBanner.style.width=(keyTotal+2)*100+"%";
+	var bannerWidth=mainBannerList[0].clientWidth;
 	var mainBannerFirst=mainBannerList[0].cloneNode(true);
 	var mainBannerLast=mainBanner.lastElementChild.cloneNode(true);
-	
-	// controller 컨트롤러
+	mainBanner.appendChild(mainBannerFirst);
+	mainBanner.insertBefore(mainBannerLast, mainBannerList[0]);
+	mainBanner.style.left="-100%";
+// controller 컨트롤러
+	var keyIndex=0;
 	var keyControl=document.querySelectorAll(".controller a");
-	
 	keyControl[0].classList.add("on");
-	
+
 	for(i=0; i<keyControl.length; i++){
 		keyControl[i].index=i;
 		keyControl[i].addEventListener("click", function(e){
 			e.preventDefault();
+			// if(bannerFlag){return false;}
 			for(j=0; j<keyControl.length; j++){
 				keyControl[j].classList.remove("on");
 			}
 			this.classList.add("on");
 			keyIndex=this.index;
-			mainBanner.style.left=keyIndex*-100+"%";
+			mainBanner.classList.remove("transNone");
+			mainBanner.style.transform="translatex("+keyIndex*-bannerWidth+"px)";
 		});
 	}
 	
 	// keyvisual 드래그 슬라이드
 	var bannerAmount=null;
 	//터치 시작
-/*	$(".mainBanner ul").on("touchstart", function(e){
-		var evt=e.originalEvent;
-		prevX=evt.touches[0].screenX;
-	//	console.log(prevX);
-	});*/
 	mainBanner.addEventListener("touchstart", function(e){
 		prevX=event.touches[0].screenX;
-		mainBanner.classList.add("transNone");
-		// console.log(prevX);
+		prevY=event.touches[0].screenY;
 	});
 	// 터치 움직임
 	mainBanner.addEventListener("touchmove", function(e){
 		moveX=event.touches[0].screenX;
+		moveY=event.touches[0].screenY;
 		bannerAmount=moveX-prevX;
-		if(keyIndex==keyTotal-1){
-			mainBanner.appendChild(mainBannerFirst);
-		}
-		else if(keyIndex==0){
-			mainBanner.insertBefore(mainBannerLast, mainBannerList[0]);
-			mainBanner.style.left=-100+"%";
-		}
-		mainBanner.style.transform="translateX("+bannerAmount+"px)";
+		
+		body.classList.add("fixed");
+		mainBanner.classList.add("transNone");
+		mainBanner.style.transform="translateX("+(keyIndex*-bannerWidth+bannerAmount)+"px)";
+
 	});
-/*	$(".mainBanner ul").on("touchmove", function(e){
-		evt=e.originalEvent;
-		moveX=evt.touches[0].screenX;
-		amount=moveX-prevX;
-		if(menuIndex==menuTotal-1 && amount<0){
-			
-		}
-		else if(menuIndex==0 && amount > 0){
-			
-		}
-		$(".sub_group").css({left:nonpix+amount});
-	});*/
-	
 	//터치 끝
 	mainBanner.addEventListener("touchend", function(e){
-		nextX=event.changedTouches[0].screenX;
+		nextX=event.changedTouches[0].screenX;	
+		body.classList.remove("fixed");
 		// console.log(nextX);
-		if(prevX-nextX> 100){ // 우측으로 슬라이드
+		if(prevX-nextX> 50){ // 우측으로 슬라이드	
 			keyIndex++;
 		}
-		else if(nextX-prevX>100){ // 좌측으로 슬라이드
-			if(keyIndex==0){
-				
-			}
+		else if(nextX-prevX> 50){ // 좌측으로 슬라이드
 			keyIndex--;
 		}
-		mainBanner.style.left=bannerAmount+"px";
-		mainBanner.style.transform="translateX(0)";
 		mainBanner.classList.remove("transNone");
-		mainBanner.style.left=keyIndex*-100+"%";
-		this.addEventListener("transitionend", function(){
-			if(keyIndex==keyTotal){
-				mainBanner.classList.add("transNone");
-				mainBanner.style.left=0;
-				mainBanner.classList.remove("transNone");
-				mainBanner.removeChild(mainBanner.lastElementChild);
-				keyIndex=0;
-			}
-			else if(keyIndex==0){
-
-				// mainBanner.classList.remove("transNone");
-				// mainBanner.removeChild(mainBanner.firstElementChild);
-				// keyIndex=0;
-			}
-		});
+		mainBanner.style.transform="translatex("+keyIndex*-bannerWidth+"px)";
+		if(keyIndex<0){
+			keyIndex=keyTotal-1;
+		}
+		else if(keyIndex==keyTotal){
+			keyIndex=0;
+		}
+		for(i=0; i<keyControl.length; i++){
+			keyControl[i].classList.remove("on");
+		}
+		keyControl[keyIndex].classList.add("on");
+		body.classList.remove("fixed");
+	});
+	mainBanner.addEventListener("transitionend", function(){
+		mainBanner.classList.add("transNone");
+		mainBanner.style.transform="translatex("+keyIndex*-bannerWidth+"px)";
+		setTimeout(function(){	
+			mainBanner.classList.remove("transNone");
+		},300);	
 	});
 	
-/*	$(".mainBanner ul").on("touchend", function(e){
-		var evt=e.originalEvent;
-		nextX=evt.changedTouches[0].screenX;
-	//	console.log(nextX);
-		if($(this).is(":animated")==true) return false
-		
-		if(prevX-nextX>50){ // 우측으로 슬라이드 
-			$(this).animate({left:"-100%"},300, function(){
-				$(this).append($(this).children().first());
-				$(this).css({left:"0"});
-			});
-		}
-		else if(nextX-prevX>50){ // 좌측으로 슬라이드
-			$(this).prepend($(this).children().last());
-			$(this).css({left:"-100%"});
-			$(this).animate({left:0},300);
-		}
-		else{
-			return false;
-		}
-	});*/
-
-/*	$(this).on("touchend", function(e){
-		evt=e.originalEvent;
-		nextX=evt.changedTouches[0].screenX;
-		
-		if(prevX-nextX>$(this).width()/2){ // 우측으로 슬라이드
-			if(menuIndex==menuTotal-1){
-				return false;
-			}
-			else{
-				menuIndex++;
-			}
-		}
-		else if(nextX-prevX>$(this).width()/2){ // 좌측으로 슬라이드
-			if(menuIndex==0){
-				return false;
-			}
-			else{
-				menuIndex--;
-			}
-		}
-		$(".food_type li").eq(menuIndex).trigger("click");
-	});*/
-// menu --------------------------------------------------------------------
+/* menu */
 	$(".food_type li").eq(0).addClass("active");
-	
-	// menu 카테고리 선택
+// menu 카테고리 선택
 	var menuIndex=0;
 	var menuTotal=$(".food_type li").length;
 	var menuWidth=$(".sub").width();
@@ -239,6 +159,9 @@ window.addEventListener("load", function(){
 
 	$(".food_type li").click(function(e){
 		e.preventDefault();
+		if($(".sub_group").is(":animated")==true){
+			return false;
+		}
 		
 		$(".sub_group").css({transform:"translateX(0)"});
 		$(".sub_group").css({left:menuDistance+menuAmount});
@@ -250,21 +173,17 @@ window.addEventListener("load", function(){
 		$(".food_type li").removeClass("active");
 		$(this).addClass("active");
 		$(".menu_scroll span").css({left:menuIndex*25+"%"});
-		$(".sub_group").animate({left:menuDistance},500);
+		$(".sub_group").animate({left:menuDistance},300);
 	});	
 	
-	//포메인메뉴 스크롤 이벤트
+//포메인메뉴 스크롤 이벤트
 	var nonpix=null; // px 제거
 	
 	$(".sub").each(function(){ // 모든 그룹마다 설정됩니다.
 		$(this).on("touchstart", function(e){
 			evt=e.originalEvent;
 			prevX=evt.touches[0].screenX;
-			res=$(".sub_group").css("left");
-			// nonpix=parseInt($(".sub_group").css("left"));
-			$("body").addClass("fixed");
 		});
-
 	//스크롤 동작
 		 $(this).on("touchmove", function(e){
 			if($(".sub_group").is(":animated")==true){
@@ -272,7 +191,10 @@ window.addEventListener("load", function(){
 			}
 			evt=e.originalEvent;
 			moveX=evt.touches[0].screenX;
-			amount=moveX-prevX;
+			menuAmount=moveX-prevX;
+			if(menuAmount!=0){
+				$("body").addClass("fixed");
+			}
 			if(menuIndex==menuTotal-1 && menuAmount<0){
 				return false;
 			}
@@ -306,15 +228,15 @@ window.addEventListener("load", function(){
 				}
 			}
 			$(".food_type li").eq(menuIndex).trigger("click");
-			$("body").removeClass("fixed");
+			$("body").removeClass("fixed");	
 		});
 	});
 
-// footer 
-	// family site
+/* footer */
+// family site
 	$(".family_site > a").click(function(e){
 		e.preventDefault();
-		if($(this).hasClass("active")){
+		if($(this).next().is(":visible")==true){
 			$(this).removeClass("active");
 			$(this).next().stop().slideUp();
 		}
@@ -323,5 +245,4 @@ window.addEventListener("load", function(){
 			$(this).next().stop().slideDown();
 		}
 	});
-	
 });
